e41fe7e28711f698fc8de51d02d7f0e1d368859e
Frederick Cheung <frederick.cheung@gmail.com>
2012-10-22 19:27:07 +0100

    state that we're using c99 for setups when it isn't the default

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,4 @@
 require 'mkmf'
-#$CFLAGS << ' -framework Security -framework CoreFoundation'
+$CFLAGS << ' -std=c99'
 $DLDFLAGS << ' -framework Security -framework CoreFoundation'
 create_makefile('keychain')
